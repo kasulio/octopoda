db0c818c7976760de10dfa61b42dc77cc9d32d65
Lukas Frey <lukas.frey03@outlook.de>
2024-11-23 14:52:01 +0100

    try using node for build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import { NextConfig } from "next";
-import "./src/env";
+import "./src/env.js";
 
-const config: NextConfig = {
+/** @type {import("next").NextConfig} */
+const config = {
   webpack: (config) => {
     config.externals.push("bun:sqlite");
     return config;
