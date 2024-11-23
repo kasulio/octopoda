c27e3d3dc6eda72481e84cdee9b88ff13b895b49
Lukas Frey <lukas.frey03@outlook.de>
2024-11-23 14:24:00 +0100

    try to fix build errors

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { NextConfig } from "next";
 import "./src/env";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const config: NextConfig = {
+  webpack: (config) => {
+    config.externals.push("bun:sqlite");
+    return config;
+  },
+};
 
 export default config;
