a632e0968df5da2bbb60d0c937689bc595c3495d
Lukas Frey <lukas.frey03@outlook.de>
2024-11-23 20:35:55 +0100

    try libsql instead

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,23 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import { Database } from "bun:sqlite";
+import { createClient, type Client } from "@libsql/client";
 
 import { env } from "~/env";
 import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/libsql";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Database | undefined;
+  // client: Database | undefined;
+  client: Client | undefined;
 };
 
+// export const client =
+//   globalForDb.client ?? new Database(env.DATABASE_PATH, { create: true });
+
 export const client =
-  globalForDb.client ?? new Database(env.DATABASE_PATH, { create: true });
+  globalForDb.client ?? createClient({ url: `file:${env.DATABASE_PATH}` });
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
