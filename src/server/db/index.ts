53036e1e70f1a5224e212607c4126f3f962e085d
Lukas Frey <lukas.frey03@outlook.de>
2024-11-23 19:42:30 +0100

    create db if it does not exist

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -12,7 +12,8 @@ const globalForDb = globalThis as unknown as {
   client: Database | undefined;
 };
 
-export const client = globalForDb.client ?? new Database(env.DATABASE_PATH);
+export const client =
+  globalForDb.client ?? new Database(env.DATABASE_PATH, { create: true });
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
