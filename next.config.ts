cdd6006cab77c05c4964fd0492a7c8e9ae13a3e8
Lukas Frey <lukas.frey03@outlook.de>
2024-11-23 20:18:26 +0100

    try fix

--- next.config.ts
+++ next.config.ts
@@ -5,16 +5,6 @@
 import { type NextConfig } from "next";
 import "./src/env";
 
-console.log("test db connection");
-// test db connection
-try {
-  const { client, db } = await import("./src/server/db");
-  console.log(client.query("SELECT 1"));
-  console.log(db.query.users.findMany());
-} catch (error) {
-  console.error(error);
-}
-
 const config: NextConfig = {};
 
 export default config;
