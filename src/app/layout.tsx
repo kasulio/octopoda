8fdadd6fd0d55efe7137af3fc90b341ec89e201b
Maja-Leonie Sodemann <maja-leonie.sodemann@student.htw-berlin.de>
2024-12-08 14:10:37 +0100

    integrated sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { GeistSans } from "geist/font/sans";
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
+import { SidebarInset } from "~/components/ui/sidebar";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
@@ -17,7 +28,30 @@ export default function RootLayout({
   return (
     <html lang="de" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <SidebarProvider>
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        Building Your Application
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+                <SidebarTrigger className="-mr-1 ml-auto rotate-180" />
+              </header>
+              {children}
+            </SidebarInset>
+            <AppSidebar />
+          </SidebarProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
