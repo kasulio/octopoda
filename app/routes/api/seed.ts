ddc86218c8f9affd4e10ffe31ce02a3b620d1ebe
Lukas Frey <lukas.frey03@outlook.de>
2025-01-14 15:50:39 +0100

    allow user seeding only once in prod

--- app/routes/api/seed.ts
+++ app/routes/api/seed.ts
@@ -1,15 +1,30 @@
 import { json } from "@tanstack/start";
 import { createAPIFileRoute } from "@tanstack/start/api";
+import humanId from "human-id";
 
 import { sqliteDb } from "~/db/client";
 import { users } from "~/db/schema";
+import { env } from "~/env";
 import { hashPassword } from "~/serverHandlers/userSession";
 
 export const APIRoute = createAPIFileRoute("/api/seed")({
   GET: async () => {
-    const uniqueId = crypto.randomUUID();
-    const email = `${uniqueId.slice(0, 8)}@test.com`;
-    const password = uniqueId.slice(8, 20);
+    const existingUsers = await sqliteDb
+      .select()
+      .from(users)
+      .limit(1)
+      .execute();
+    if (env.PUBLIC_NODE_ENV === "production" && existingUsers.length > 0) {
+      return json({
+        error: "User already exists",
+      });
+    }
+
+    const uniqueId = humanId({ separator: "-", capitalize: false });
+    const password = humanId({ separator: "-", capitalize: false });
+
+    const email = `${uniqueId}@test.com`;
+
     await sqliteDb.insert(users).values({
       passwordHash: await hashPassword(password),
       email,
