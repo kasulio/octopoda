ac47a89cded9fc41a73a9020b6dfae943c82a6df
Lukas Frey <lukas.frey03@outlook.de>
2025-01-23 00:13:56 +0100

    fix default time range start

--- app/constants.ts
+++ app/constants.ts
@@ -11,7 +11,7 @@ export const possibleInstanceTimeSeriesMetrics = [
 ] as const;
 
 export const getTimeRangeDefaults = () => ({
-  start: new Date().setHours(0, 0, 0, 0) - 7 * 24 * 60 * 60 * 1000,
+  start: +new Date() - 7 * 24 * 60 * 60 * 1000,
   end: +new Date(),
   windowMinutes: 10,
 });
