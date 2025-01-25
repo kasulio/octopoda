279ae3659faf4a1918e4373fea9d4f543ede8476
pichi <nick@kriesing.de>
2025-01-25 13:55:55 +0100

    testing login fields existence

---
+++ tests/login_tests/login.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "@playwright/test";
+
+import { LoginPage } from "./login.po";
+
+
+
+test("has title", async ({ page }) => {
+  await page.goto("/dashboard");
+
+  const loginPage = new LoginPage(page);
+  // Expect a title "to contain" a substring.
+  await expect(loginPage.usernameInputfield).toBeVisible();
+  await loginPage.usernameInputfield.fill("admin");
+  await expect(loginPage.passwordInputfield).toBeVisible();
+  await loginPage.passwordInputfield.fill("admin");
+  await expect(loginPage.loginButton).toBeVisible();
+  await loginPage.loginButton.click();
+});
